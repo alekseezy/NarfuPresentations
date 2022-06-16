dd3f83daf7856d606caf0f7444d06c9927fed1d1
Gamzakov <13555583+Gamzakov@users.noreply.github.com>
2022-06-16 21:00:17 +0300

    Initialize Project
    * Create folder sctructure
    * Create projects in coresponding folders
    * Setup cross-project dependencies

---
+++ src/UI/NarfuPresentations.UI.MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NarfuPresentations.UI.MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
